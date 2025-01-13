8fa9faad435d14edfe3e7d7c2a6943c6adc3b108
Yannick Müller <info@yannickmueller.dev>
2025-01-13 13:30:31 +0100

    added nvim files

---
+++ nvim/lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+--
+vim.o.expandtab = false
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
